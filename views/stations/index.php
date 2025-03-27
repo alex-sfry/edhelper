fb5e469500532c636f06a6d9ff9f7e554c19b113
Aleksandr Tochilin <alex8079@gmail.com>
2025-03-27 03:41:17 +0300

    hide form begind filter button

--- views/stations/index.php
+++ views/stations/index.php
@@ -15,99 +15,109 @@ AutocompleteAsset::register($this);
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="stations-index container-xxl text-light mt-3">
-
     <h1 class="text-center"><?= $this->title ?></h1>
-
-    <div class="row justify-content-center mt-3">
+    <div class="row mt-3">
         <div class="col-12 col-md-9 col-lg-8">
-            <div class="stations-search row justify-content-center">
-                <div class="col-12">
-                    <?php $form = ActiveForm::begin([
-                        'id' => 'stations-form',
-                        'action' => Url::to(['stations/index']),
-                        'method' => 'get',
-                        'successCssClass' => null,
-                        'options' => ['class' => 'py-1 px-2 mb-3', 'data-bs-theme' => 'dark', 'novalidate' => '']
-                    ]); ?>
-                    <div class="row">
-                        <div class="col-12 col-md-6">
-                            <div class="ui-front ui-widget">
-                                <div class="d-flex align-items-end">
-                                    <?= $form
-                                        ->field(
-                                            $searchModel,
-                                            'refSystem',
-                                            [
-                                                'options' => ['class' => 'mb-3 w-100'],
-                                                'labelOptions' => ['class' => 'form-label']
-                                            ]
-                                        )
-                                        ->textInput([
-                                            'data-bs-theme' => 'dark',
-                                            'autocomplete' => 'off'
-                                        ]) ?>
-                                    <div class="spinner spinner-border spinner-border-sm text-light visually-hidden"
-                                        role="status">
+            <div class="stations-search d-flex">
+                <div>
+                    <button id="filter-btn"
+                        class="btn btn-secondary lsp-01 mb-1"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseForm"
+                        aria-expanded="false"
+                        aria-controls="collapseForm">
+                        filter
+                    </button>
+                </div>
+                <div id="collapseForm" class="form-filter collapse row w-100 ms-1">
+                    <div class="col-12">
+                        <?php $form = ActiveForm::begin([
+                            'id' => 'stations-form',
+                            'action' => Url::to(['stations/index']),
+                            'method' => 'get',
+                            'successCssClass' => null,
+                            'options' => ['class' => 'py-1 px-2 mb-3', 'data-bs-theme' => 'dark', 'novalidate' => '']
+                        ]); ?>
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="ui-front ui-widget">
+                                    <div class="d-flex align-items-end">
+                                        <?= $form
+                                            ->field(
+                                                $searchModel,
+                                                'refSystem',
+                                                [
+                                                    'options' => ['class' => 'mb-3 w-100'],
+                                                    'labelOptions' => ['class' => 'form-label']
+                                                ]
+                                            )
+                                            ->textInput([
+                                                'data-bs-theme' => 'dark',
+                                                'autocomplete' => 'off'
+                                            ]) ?>
+                                        <div class="spinner spinner-border spinner-border-sm text-light visually-hidden"
+                                            role="status">
+                                        </div>
                                     </div>
                                 </div>
+                                <?= $form
+                                    ->field($searchModel, 'minPadSize', ['labelOptions' => ['class' => 'form-label']])
+                                    ->radioList(
+                                        ['' => 'S/M', 'L' => 'L'],
+                                        [
+                                            'class' => 'd-flex',
+                                            'separator' => '<span class="mx-2"></span>',
+                                            'data-bs-theme' => 'dark',
+                                            'itemOptions' => ['labelOptions' => ['class' => 'form-check-label']],
+                                            'unselect' => null
+                                        ]
+                                    ) ?>
+                                <?= $form
+                                    ->field($searchModel, 'inclSurface', ['labelOptions' => ['class' => 'form-label']])
+                                    ->radioList(
+                                        ['No' => 'No', 'Yes' => 'Yes (w/o Odyssey)'],
+                                        [
+                                            'class' => 'd-flex',
+                                            'separator' => '<span class="mx-2"></span>',
+                                            'data-bs-theme' => 'dark',
+                                            'itemOptions' => ['labelOptions' => ['class' => 'form-check-label']],
+                                            'unselect' => null
+                                        ]
+                                    ) ?>
                             </div>
-                            <?= $form
-                                ->field($searchModel, 'minPadSize', ['labelOptions' => ['class' => 'form-label']])
-                                ->radioList(
-                                    ['' => 'S/M', 'L' => 'L'],
-                                    [
-                                        'class' => 'd-flex',
-                                        'separator' => '<span class="mx-2"></span>',
-                                        'data-bs-theme' => 'dark',
-                                        'itemOptions' => ['labelOptions' => ['class' => 'form-check-label']],
-                                        'unselect' => null
-                                    ]
-                                ) ?>
-                            <?= $form
-                                ->field($searchModel, 'inclSurface', ['labelOptions' => ['class' => 'form-label']])
-                                ->radioList(
-                                    ['No' => 'No', 'Yes' => 'Yes (w/o Odyssey)'],
-                                    [
-                                        'class' => 'd-flex',
-                                        'separator' => '<span class="mx-2"></span>',
-                                        'data-bs-theme' => 'dark',
-                                        'itemOptions' => ['labelOptions' => ['class' => 'form-check-label']],
-                                        'unselect' => null
-                                    ]
-                                ) ?>
+                            <div class="col-12 col-md-6">
+                                <?= $form
+                                    ->field($searchModel, 'economyId1', ['labelOptions' => ['class' => 'form-label']])
+                                    ->dropDownList(
+                                        [
+                                            "" => "Any",
+                                            1 => "Agriculture",
+                                            2 => "Colony",
+                                            3 => "Extraction",
+                                            4 => "High Tech",
+                                            5 => "Industrial",
+                                            6 => "Military",
+                                            8 => "Refinery",
+                                            9 => "Service",
+                                            10 => "Terraforming",
+                                            11 => "Tourism",
+                                        ],
+                                        ['class' => 'form-select', 'data-bs-theme' => 'dark']
+                                    ) ?>
+                                <?= $form
+                                    ->field(
+                                        $searchModel,
+                                        'distance_to_arrival',
+                                        ['labelOptions' => ['class' => 'form-label']]
+                                    )
+                                    ->textInput(['data-bs-theme' => 'dark']) ?>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-6">
-                            <?= $form
-                                ->field($searchModel, 'economyId1', ['labelOptions' => ['class' => 'form-label']])
-                                ->dropDownList(
-                                    [
-                                        "" => "Any",
-                                        1 => "Agriculture",
-                                        2 => "Colony",
-                                        3 => "Extraction",
-                                        4 => "High Tech",
-                                        5 => "Industrial",
-                                        6 => "Military",
-                                        8 => "Refinery",
-                                        9 => "Service",
-                                        10 => "Terraforming",
-                                        11 => "Tourism",
-                                    ],
-                                    ['class' => 'form-select', 'data-bs-theme' => 'dark']
-                                ) ?>
-                            <?= $form
-                                ->field(
-                                    $searchModel,
-                                    'distance_to_arrival',
-                                    ['labelOptions' => ['class' => 'form-label']]
-                                )
-                                ->textInput(['data-bs-theme' => 'dark']) ?>
+                        <div class="text-center mb-1">
+                            <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
                         </div>
+                        <?php ActiveForm::end(); ?>
                     </div>
-                    <div class="text-center mb-1">
-                        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-                    </div>
-                    <?php ActiveForm::end(); ?>
                 </div>
             </div>
         </div>
