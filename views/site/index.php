57465bfe7e1f501982e75d978de62946fece9fae
Aleksandr Tochilin <alex8079@gmail.com>
2025-04-09 11:18:36 +0300

    create home page

--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,31 @@
 
 /** @var yii\web\View $this */
 
+use yii\bootstrap5\Html;
+
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<div class="container-xxl mt-5">
+    <div class="row flex-column align-content-center px-2">
+        <div class="col-lg-4 col-sm-6 p-2">
+            <?= Html::a('economies', ['economies/index'], [
+                'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            ]) ?>
         </div>
-
+        <div class="col-lg-4 col-sm-6 p-2">
+            <?= Html::a('engineers', ['engineers/index'], [
+                'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            ]) ?>
+        </div>
+        <div class="col-lg-4 col-sm-6 p-2">
+            <?= Html::a('material traders', ['material-traders/index'], [
+                'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            ]) ?>
+        </div>
+        <div class="col-lg-4 col-sm-6 p-2">
+            <?= Html::a('stations', ['stations/index'], [
+                'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            ]) ?>
+        </div>
     </div>
 </div>
