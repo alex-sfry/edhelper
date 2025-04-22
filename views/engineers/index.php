82a669fe7acc277ebc623fb19c1f1aca5524e5f5
Aleksandr Tochilin <alex8079@gmail.com>
2025-04-22 12:22:51 +0300

    sort upgradedes g5->g1, highlight g5 upgrades

--- views/engineers/index.php
+++ views/engineers/index.php
@@ -1,6 +1,10 @@
 <?php
 
 /** @var yii\web\View $this */
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
 /** @var app\models\Engineers[] $data */
 
 $th = ['name', 'upgrades', 'system', 'station', 'discovery', 'invite', 'unlock'];
@@ -16,34 +20,51 @@ $this->title = 'Engineers';
         </div>
     </div>
     <div class="table-responsive rounded-1">
-        <table id="dataTable"
-            class="table table-striped">
+        <table id="dataTable" class="table table-striped">
             <thead>
                 <tr>
                     <?php foreach ($th as $item) : ?>
-                    <th
-                        scope="col"
-                        class="text-orange border-bottom border-secondary">
-                        <?= ucfirst($item) ?>
-                    </th>
+                        <th scope="col" class="text-orange border-bottom border-secondary">
+                            <?= ucfirst($item) ?>
+                        </th>
                     <?php endforeach; ?>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($data as $item) : ?>
-                <tr>
-                    <td><?= $item->name ?></td>
-                    <td>
-                        <?php foreach ($item->engineersUpgrades as $val) : ?>
-                        <span class="text-decoration-underline"><?= $val->upgrade ?></span><br>
-                        <?php endforeach; ?>
-                    </td>
-                    <td><?= $item->system->name ?></td>
-                    <td><?= $item->station->name ?></td>
-                    <td><?= $item->discovery ?></td>
-                    <td><?= $item->get_invite ?></td>
-                    <td><?= $item->unlock ?></td>
-                </tr>
+                    <tr>
+                        <td class="text-nowrap"><?= $item->name ?></td>
+                        <td class="text-nowrap">
+                            <?php
+                            $upgrades = ArrayHelper::getColumn(
+                                ArrayHelper::toArray($item->engineersUpgrades),
+                                'upgrade'
+                            );
+
+                            usort($upgrades, function ($a, $b) {
+                                preg_match('/G(\d)/', $a, $matchA);
+                                preg_match('/G(\d)/', $b, $matchB);
+                                $gradeA = isset($matchA[1]) ? (int)$matchA[1] : 0;
+                                $gradeB = isset($matchB[1]) ? (int)$matchB[1] : 0;
+
+                                return $gradeB <=> $gradeA; // Descending order
+                            });
+
+                            foreach ($upgrades as $val) {
+                                $options = ['class' => 'text-decoration-underline'];
+                                str_contains($val, 'G5') && Html::addCssClass($options, 'fw-bold');
+                                echo Html::beginTag('span', $options);
+                                    echo $val . '<br>';
+                                echo Html::endTag('span');
+                            }
+                            ?>
+                        </td>
+                        <td><?= $item->system->name ?></td>
+                        <td><?= $item->station->name ?></td>
+                        <td><?= $item->discovery ?></td>
+                        <td><?= $item->get_invite ?></td>
+                        <td><?= $item->unlock ?></td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
