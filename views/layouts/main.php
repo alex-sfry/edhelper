7dcb05a964a5456945e15659b4535ccd9d83b425
Aleksandr Tochilin <alex8079@gmail.com>
2025-05-15 09:24:48 +0300

    fhfghfg

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,36 +32,50 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <body class="lsp-01 d-flex flex-column overflow-x-hidden h-100">
     <?php $this->beginBody() ?>
     <header class="edh-header">
-        <nav class="navbar navbar-expand-lg bg-layout">
-            <div class="container-xxl">
-                <div>
-                    <a class="navbar-brand text-orange fs-1 fw-bold" href="/trading/">
-                        EDH
-                    </a>
-                </div>
-                <button
-                    class="navbar-toggler bg-orange"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    class="collapse navbar-collapse text-center d-lg-flex justify-content-center me-lg-5"
-                    id="navbarNav">
-                    <ul class="navbar-nav me-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link active text-uppercase fw-bold" aria-current="page" href="/trading/">
-                                Home
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php
+        NavBar::begin([
+            'brandLabel' => 'EDH',
+            'options' => ['class' => 'navbar navbar-expand-lg bg-layout'],
+            'brandOptions' => ['class' => 'text-orange fs-2 fw-bold'],
+            'togglerOptions' => ['class' => 'text-primary bg-orange'],
+        ]);
+        echo Nav::widget([
+            'items' => [
+                // [
+                //     'label' => 'Home',
+                //     'url' => ['site/index'],
+                //     'linkOptions' => ['class' => 'nav-link fs-5 fw-bold']
+                // ],
+                [
+                    'label' => 'Economies',
+                    'url' => ['economies/index'],
+                    'linkOptions' => ['class' => 'nav-link fs-5 fw-bold']
+                ],
+                [
+                    'label' => 'Trading',
+                    'url' => ['trading/index'],
+                    'linkOptions' => ['class' => 'nav-link fs-5 fw-bold ms-lg-1']
+                ],
+                [
+                    'label' => 'Engineers',
+                    'url' => ['engineers/index'],
+                    'linkOptions' => ['class' => 'nav-link fs-5 fw-bold ms-lg-1']
+                ],
+                [
+                    'label' => 'Material traders',
+                    'url' => ['material-traders/index'],
+                    'linkOptions' => ['class' => 'nav-link fs-5 fw-bold ms-lg-1']
+                ],
+                [
+                    'label' => 'Stations',
+                    'url' => ['stations/index'],
+                    'linkOptions' => ['class' => 'nav-link fs-5 fw-bold ms-lg-1']
+                ],
+            ],
+            'options' => ['class' => 'navbar-nav m-auto text-center'],
+        ]);
+        NavBar::end();
+        ?>
     </header>
     <main class="flex-shrink-0 flex-grow-1 bg-main">
         <div class="container-xxl">
