955472ce399d01ae67b6b21c0af8ee0e4934dac9
Aleksandr Tochilin <alex8079@gmail.com>
2025-05-15 07:45:16 +0300

    add trading button, reduce sizeof buttons

--- views/site/index.php
+++ views/site/index.php
@@ -9,22 +9,27 @@ $this->title = 'Elite Dangerous Helper';
 <div class="row flex-column align-content-center px-2 mt-5">
     <div class="col-lg-4 col-sm-6 p-2">
         <?= Html::a('economies', ['economies/index'], [
-            'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            'class' => 'btn btn-success w-100 text-center py-3 fw-bold text-nowrap text-uppercase'
+        ]) ?>
+    </div>
+    <div class="col-lg-4 col-sm-6 p-2">
+        <?= Html::a('trading', ['trading/index'], [
+            'class' => 'btn btn-success w-100 text-center py-3 fw-bold text-nowrap text-uppercase'
         ]) ?>
     </div>
     <div class="col-lg-4 col-sm-6 p-2">
         <?= Html::a('engineers', ['engineers/index'], [
-            'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            'class' => 'btn btn-success w-100 text-center py-3 fw-bold text-nowrap text-uppercase'
         ]) ?>
     </div>
     <div class="col-lg-4 col-sm-6 p-2">
         <?= Html::a('material traders', ['material-traders/index'], [
-            'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            'class' => 'btn btn-success w-100 text-center py-3 fw-bold text-nowrap text-uppercase'
         ]) ?>
     </div>
     <div class="col-lg-4 col-sm-6 p-2">
         <?= Html::a('stations', ['stations/index'], [
-            'class' => 'btn btn-success w-100 text-center px-1 py-5 fw-bold text-nowrap text-uppercase'
+            'class' => 'btn btn-success w-100 text-center py-3 fw-bold text-nowrap text-uppercase'
         ]) ?>
     </div>
 </div>
